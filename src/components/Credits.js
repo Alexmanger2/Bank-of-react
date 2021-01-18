7c6edcd2acf013706fa3df99a02e175249490b64
Alexmanger2 <Alexmanger2@gmail.com>
2021-01-18 15:39:13 -0500

    initial commit

---
+++ src/components/Credits.js
@@ -0,0 +1,14 @@
+  
+import React, {Component} from 'react';
+
+class Credits extends Component {
+  render() {
+    return (
+        <div>
+          Credits: {this.props.accountCredits}
+        </div>
+    );
+  }
+}
+
+export default Credits;
